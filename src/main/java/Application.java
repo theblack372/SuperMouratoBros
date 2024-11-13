3352a096edda8f5b00d102859fb54a27090cb8d4
theblack372 <pedroasrpinto@gmail.com>
2024-11-13 15:04:27 +0000

    terminal appears in screen

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,44 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        Game game = new Game();
+        game.run();
+    }
+}
+
+class Game{
+    private boolean endTerminal=false;
+    private Screen screen;
+
+    public Game() {
+        try {
+            // Set a fixed terminal size
+            TerminalSize fixedSize = new TerminalSize(80, 24); // for example, 80 columns by 24 rows
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(fixedSize);
+
+            screen = terminalFactory.createScreen();
+            screen.startScreen();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void run() throws IOException {
+        while (!endTerminal) {
+            KeyStroke key = screen.readInput();
+            if (key.getKeyType() == KeyType.Character && key.getCharacter()== 'q') {
+                screen.stopScreen();
+                endTerminal = true;
+            }
+        }
     }
 }
